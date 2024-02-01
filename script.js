8eef60c72417ddacb794fac3e22f5e7108250435
lsherman2 <logansherman1@gmail.com>
2024-01-31 20:56:07 -0600

    Songs are now sorted alphabetically.

--- script.js
+++ script.js
@@ -299,5 +299,17 @@ audio.addEventListener("ended", () => {
   }
 });
 
+userData?.songs.sort((a,b) => {
+  if (a.title < b.title) {
+    return -1;
+  }
+
+  if (a.title > b.title) {
+    return 1;
+  }
+
+  return 0;
+});
+
 renderSongs(userData?.songs);
 setPlayButtonAccessibleText();
